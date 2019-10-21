44c8b6077afc364b6bd09b73d49d1491bc56660e
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-10-21 13:04:47 -0600

    created login page

---
+++ veganmeets/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Header = () => {
+
+return (
+    <div className="headerContainer">
+        <h1><span className="veganheader">Vegan</span>Meets</h1>
+    </div>
+)
+}
+
+export default Header
